9e58a18651bc9df40a5105973d271a666405214b
isayani <isayani95@gmail.com>
2022-07-23 16:51:16 -0400

    third commit all prompts functional

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+// TO DO:
+// create series of on-click prompts
+
+
 // Query selector for the button
 var generateBtn = document.querySelector("#generate");
 
@@ -5,10 +9,51 @@ var generateBtn = document.querySelector("#generate");
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 var symbols = ["!","#","$","%","&","*","(",")","@"];
 var characterCodes = Array.from(Array(26)).map((_,i) => i +97);
-console.log(characterCodes);
 var lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
 var uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
 
+// Prompts and Password Generating If/Else Statements
+function generatePassword() {
+  var lengthChar = prompt("Please enter a password length between 8 and 128");
+
+  if(!lengthChar ) {
+    alert("You must enter a value");
+  } else if (lengthChar < 8 || lengthChar > 128){
+    alert("Password must be between 8 and 128!");
+  } else {
+    var confirmNum = confirm("Do you want numbers in your password?");
+    var confirmUpper = confirm("Do you want uppercase letters in your password?");
+    var confirmLower = confirm("Do you want lowercase letters in your password?");
+    var confirmSpecial = confirm("Do you want special characters in your password?");
+  };
+  
+  if (lengthChar > 8 && lengthChar < 128 && !confirmNum && !confirmUpper && !confirmLower && !confirmSpecial) {
+    alert("You must choose at least one criteria!");
+  } else if (confirmNum && confirmUpper && confirmLower && confirmSpecial) {
+    var writePassword = (length, hasNumbers, hasSymbols, hasLowercase, hasUppercase) =>{
+        var availableCharacters = [
+          ...(hasSymbols ? symbols : []),
+          ...(hasNumbers ? numbers : []),
+          ...(hasLowercase ? lowercaseLetters : []),
+          ...(hasUppercase ? uppercaseLetters : []),
+        ];
+      
+        var hiddenPassword = "";
+        
+        if(availableCharacters.length === 0) return "";
+        
+        for(var i = 0; i <length; i++){
+          var randomIndex = Math.floor(Math.random() * availableCharacters.length);
+          hiddenPassword += availableCharacters[randomIndex];
+        }
+        return hiddenPassword;
+      }
+  }
+}
+
+// Conditional Operator Statements
+
+console.log(generatePassword(12, true, true, true, true));
 
 // Write password to the #password input
 function writePassword() {
@@ -18,36 +63,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
-
-
-
-
-
-
-
-// Conditional Operator Statements
-var writePassword = (length, hasNumbers, hasSymbols, hasLowercase, hasUppercase) =>{
-  var availableCharacters = [
-    ...(hasSymbols ? symbols : []),
-    ...(hasNumbers ? numbers : []),
-    ...(hasLowercase ? lowercaseLetters : []),
-    ...(hasUppercase ? uppercaseLetters : []),
-  ];
-
-  var hiddenPassword = "";
-  
-  if(availableCharacters.length === 0) return "";
-  
-  for(var i = 0; i <length; i++){
-    var randomIndex = Math.floor(Math.random() * availableCharacters.length);
-    hiddenPassword += availableCharacters[randomIndex];
-  }
-  return hiddenPassword;
-}
-
-console.log(writePassword(12, true, true, true, true));
-   
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
